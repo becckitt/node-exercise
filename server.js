06a5bbc8f029bd0f21e77fff547e09ba3914342e
Glitch (rust-client) <none>
2019-06-14 02:43:48 +0000

    🦁🗾 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,15 @@
+// init project
+
+// http://expressjs.com/en/starter/static-files.html
+
+// init sqlite db
+var fs = require('fs');
+var dbFile = './.data/sqlite.db';
+var exists = fs.existsSync(dbFile);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(dbFile);
+
+// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
+db.serialize(function(){
+
+});
